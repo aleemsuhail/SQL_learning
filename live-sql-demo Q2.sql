781c24eea215aea0d717a8e6288d2b48a228f8f7
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2025-07-08 12:44:58 +0530

    Add files via upload
    Using WHERE, OR operators.

---
+++ live-sql-demo Q2.sql
@@ -0,0 +1,11 @@
+Q2.Write a SQL query to fetch the student name who has scored 90 or appeared in Che.
+Create table Student(Studid number,NAME varchar2(10),Subject varchar2(20),marks number);
+insert into student values(1,'A','Phy','90');
+insert into student values(1,'A','Che','95');
+insert into student values(2,'B','Phy','80');
+insert into student values(2,'B','Che','85');
+insert into student values(3,'C','Phy','90');
+insert into student values(4,'D','Phy','75');
+insert into student values(4,'D','Che','90');
+
+Select NAME from Student Where marks = 90 or Subject = 'Che';
